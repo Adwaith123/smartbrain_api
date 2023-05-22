e3836be5249e2fd849999ee9543455c821048f50
Adwaith Rajesh <64030486+Adwaith123@users.noreply.github.com>
2023-05-22 21:06:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
+    host : 'dpg-chlojnu7avj217ft4g30-a',
+    user : 'adwaith',
     password: 'root',
-    database : 'smartbrain'
+    database : 'smartbrain_zysn'
   }
 });
 
