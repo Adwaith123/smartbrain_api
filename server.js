1a142dd3ad67cbdea9ee2ec17ca18a6a8050e3f9
Adwaith Rajesh <64030486+Adwaith123@users.noreply.github.com>
2023-05-22 22:09:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connectionString: {
-    host : process.env.DATABASE_URL,
-    ssl:true,
+    connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl:{rejectUnauthorized: false},
+    host:process.env.DATABASE_HOST,
+    port:5432,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PW,
+    database:process.env.DATABASE_DB
   }
 });
 
